7008afb195a331714c43d3ed19b52972d4eea0f1
arman18 <bsse1029@iit.du.ac.bd>
2022-11-06 17:48:40 +0600

    combination_edited

--- combination_accuracy.py
+++ combination_accuracy.py
@@ -7,7 +7,7 @@ Created on Sun Nov  6 15:17:00 2022
 from dataset_feature_preprocessor import pre_process
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-
+from sklearn.model_selection import train_test_split
 def combination(df,category,lastCol):
     
     for col in category:
@@ -21,18 +21,71 @@ def combination(df,category,lastCol):
             if col == lastCol:
                 return (category, False)
 
+def get_trin_df(df,category):
+    for col in category:
+        idx = category[col]
+        idx -=1
+        if idx < 0:
+            df = df[df[col] !=-1]
+            continue;
+        df = df[df[col] == df[col].unique()[idx]]
+    return df
+
+def add_row_to_df(df,accdf,accuracy,category):
+    values = [str(accuracy)]
+    for col in category:
+        idx = category[col]
+        idx -=1
+        if idx < 0:
+            values.append('Any')
+            continue;
+        values.append(df[col].unique()[idx])
+        
+    accdf.loc[len(accdf)] = values
+    return accdf
+
+def create_empty_df(category):
+    obj = {}
+    obj["Accuracy"] = []
+    for col in category:
+        obj[col] = []
+    return pd.DataFrame(obj)
 
 if __name__ == '__main__':
     
     data = pd.read_csv('data/original_garibazar_dataset.csv')
     
     data = pre_process(data)
+    
     df =data
     df = df.drop(['Created_date', 'Phone'],axis=1)
     
-    category = {'Gearbox':0,'Color':0}
+    # ----------- deleting other numeric -------------
+    
+    # df = df.drop(['Engine', 'Year'],axis=1)
+    # numeric = 'Mileage'
+    
+    # df = df.drop(['Engine', 'Mileage'],axis=1)
+    # numeric = 'Year'
+    
+    # df = df.drop(['Year', 'Mileage'],axis=1)
+    # numeric = 'Engine'
+    # --------------- end -----------------------------
+    
+    onlycat = ["price",'Gearbox','Color','Body_type','Fuel_type','Air_condition','Drive_type','Condition','Model','Location']
+    category = {'Gearbox':0,'Color':0,'Body_type':0,'Fuel_type':0,'Air_condition':0,'Drive_type':0,'Condition':0,'Model':0,'Location':0}
+    accdf = create_empty_df(category)
     avail = True
     while avail:
-        print(category)
+        traindf = get_trin_df(df, category)
+        X = traindf.drop(onlycat, axis=1)
+        y = traindf['price']
+        
+        X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=10)
+        
+        lr = LinearRegression()
+        lr.fit(X_train,y_train)
+        score = lr.score(X_test,y_test)
+        accdf = add_row_to_df(df,accdf,score,category)
         category,avail = combination(df,category,'Color')
         
